7b102233e004f81e6ff866e3f42ac6825fe7bd34
Shreyash-jiwane09 <jiwane.shrey09@gmail.com>
2025-03-05 16:54:21 +0530

    Pickle file created

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,10 +5,13 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
-
+from src.components.data_transformation import DataTransformation
 
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     print(train_data_path,test_data_path)
+    
+    data_transformation = DataTransformation()
+    train_arr,test_arr,obj_path=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
     
